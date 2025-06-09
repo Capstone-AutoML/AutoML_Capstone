0e880b8b8b1ea8ef63fa390444791c7278a40c77
elshaday <elshadayrn13@gmail.com>
2025-06-09 09:22:14 -0700

    added auto download for distillation

--- fetch_dataset.sh
+++ fetch_dataset.sh
@@ -25,7 +25,7 @@ ZIP_MODEL="mock_io/model_registry/model_weights.zip"
 IMAGE_DIR="mock_io/data/sampled_dataset/images"
 MODEL_DIR="mock_io/model_registry/model"
 
-DATA_ASSET_ID=259237441
+DATA_ASSET_ID=262360637
 MODEL_ASSET_ID=259250340
 
 # Dataset
@@ -56,4 +56,23 @@ if [ ! -d "$MODEL_DIR" ]; then
     rm "$ZIP_MODEL"
 else
     echo "✅ Model weights already exist."
+fi
+
+# Distillation dataset
+DISTILLATION_DIR="mock_io/data/distillation/distillation_dataset"
+ZIP_DISTILLATION="mock_io/data/distillation_dataset.zip"
+DISTILLATION_ASSET_ID=262351896
+
+if [ ! -d "$DISTILLATION_DIR" ]; then
+    echo "📥 Downloading distillation dataset..."
+    mkdir -p "mock_io/data/distillation"
+    curl -L \
+      -H "Authorization: token $GITHUB_TOKEN" \
+      -H "Accept: application/octet-stream" \
+      -o "$ZIP_DISTILLATION" \
+      https://api.github.com/repos/Elshaday-Tamire/capstone-assets/releases/assets/$DISTILLATION_ASSET_ID
+    unzip "$ZIP_DISTILLATION" -d "mock_io/data/distillation/"
+    rm "$ZIP_DISTILLATION"
+else
+    echo "✅ Distillation dataset already exists."
 fi
